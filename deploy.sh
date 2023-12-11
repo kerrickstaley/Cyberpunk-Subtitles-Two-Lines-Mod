15455db6665768c8a91d97156241efbb7360feb5
Kerrick Staley <k@kerrickstaley.com>
2023-12-10 19:05:08 -0500

    Update deploy path

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scp init.lua deck:".local/share/Steam/steamapps/common/Cyberpunk 2077/bin/x64/plugins/cyber_engine_tweaks/mods/subs-two-lines/init.lua"
+scp init.lua deck:".local/share/Steam/steamapps/common/Cyberpunk 2077/bin/x64/plugins/cyber_engine_tweaks/mods/subtitles-two-lines/init.lua"
